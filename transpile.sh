84e6f6881b4fd732ba135a72bb2621f46839b0a9
James-Mart <jmartx93@gmail.com>
2025-04-22 19:06:17 +0000

    compile the component before transpilation

--- transpile.sh
+++ transpile.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+cd wasm
+cargo component build -r
+cd ..
 yarn jco transpile ./wasm/target/wasm32-wasip1/release/wasm.wasm --minify --no-nodejs-compat --base64-cutoff=4096 --valid-lifting-optimization -o ./wasm-transpiled
